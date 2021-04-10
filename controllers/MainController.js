28cf721a735f7e252a1bfeb9bfba8e4ea0b60bd7
Yudho Alfantyo <nosvengeance@gmail.com>
2021-04-10 22:49:38 +0700

    Index Mahasiswa

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -3,6 +3,16 @@
 const response = require('../res');
 const connection = require('../connection');
 
-exports.index = function(req, res) {
+exports.test = function(req, res) {
     response.ok('REST API App is running.', res);
 };
+
+exports.index = (req, res) => {
+    connection.query('SELECT * FROM mahasiswa', (error, rows, fields) => {
+        if(error) {
+            connection.log(error);
+        }else {
+            response.ok(rows, res);
+        }
+    });
+}
--- routes/routes.js
+++ routes/routes.js
@@ -3,5 +3,6 @@
 module.exports = function(app) {
     const jsonku = require('../controllers/MainController');
 
-    app.route('/').get(jsonku.index);
+    app.route('/').get(jsonku.test);
+    app.route('/mahasiswa').get(jsonku.index);
 };
